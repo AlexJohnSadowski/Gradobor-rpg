a01176b22f62c5216dc1f212e5c8c645165eba06
unknown <sadovsky.aleksander@gmail.com>
2020-07-02 02:34:16 +0200

    Initial commit

--- src/config/fbConfig.js
+++ src/config/fbConfig.js
@@ -1,16 +1,17 @@
 import firebase from 'firebase/app'
 import 'firebase/firestore'
 import 'firebase/auth'
+require('dotenv').config()
 
 var firebaseConfig = {
-    apiKey: "AIzaSyCEuIPJnnxjWmbnWJ_eo70YcFKlsDd26us",
-    authDomain: "gradobor-rpg.firebaseapp.com",
-    databaseURL: "https://gradobor-rpg.firebaseio.com",
-    projectId: "gradobor-rpg",
-    storageBucket: "gradobor-rpg.appspot.com",
-    messagingSenderId: "360554453268",
-    appId: "1:360554453268:web:40f7184988116877e0ca01",
-    measurementId: "G-JC5K6N1JKZ"
+    apiKey: process.env.API_KEY,
+    authDomain: process.env.AUTH_DOMAIN,
+    databaseURL: process.env.DATABASE_URL,
+    projectId: process.env.PROJECT_ID,
+    storageBucket: process.env.STORAGE_BUCKET,
+    messagingSenderId: process.env.MESSAGING_SENDER_ID,
+    appId: process.env.APP_ID,
+    measurementId: process.env.MEASUREMENT_ID,
   };
   // Initialize Firebase
   firebase.initializeApp(firebaseConfig);
